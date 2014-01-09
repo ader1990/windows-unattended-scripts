330e8d9b9e542eabb721c6d796724602e4792d0d
Marco Fontana <mfontana@cloudbasesolutions.com>
2014-01-09 18:01:53 +0100

    added registry registration, added logging

--- ps/unattended_exchange2013.ps1
+++ ps/unattended_exchange2013.ps1
@@ -1,6 +1,5 @@
 param($Step="Prepare",
 $domain = "FONTOHOME",
-$domainsuffix ="LOCAL",
 $dcusername = "administrator",
 $dcpassword = "FontoMarco1982!",
 $setupPath = "d:\setup.exe",
@@ -11,6 +10,7 @@ $global:startingStep = $Step
 $global:restartKey = "Restart-And-Resume"
 $global:RegRunKey ="HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
 $global:powershell = (Join-Path $env:windir "system32\WindowsPowerShell\v1.0\powershell.exe")
+$tempFolder = "c:\Windows\temp\"
 
 
 function Should-Run-Step([string] $prospectStep) 
@@ -76,12 +76,21 @@ function DependencyInstall($url, $filename) {
         del $filename
 }
 
+$logFile = $tempFolder + "install_ex2013_log.txt"
+if(!(Test-Path -Path $logFile )){
+    New-Item $logFile -type file
+}
+function log([string] $message){
+    Add-Content $logFile $message
+}
+
 
 $script = $myInvocation.MyCommand.Definition
 Clear-Any-Restart
 if (Should-Run-Step "Prepare")
 {
    #using old local administrator account
+   log "registering local admin as autologon, saving vars in registry"
    $localadmin = [ADSI]'WinNT://./Administrator'
    $localadmin.SetPassword($dcpassword)
    New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoAdminLogon -Value 1 
@@ -90,12 +99,28 @@ if (Should-Run-Step "Prepare")
    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoAdminLogon -Value 1 
    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultUserName -Value "Administrator"
    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultPassword -Value $dcpassword
+   New-Item -Path Registry::HKLM\SOFTWARE\Unattended
+   New-ItemProperty -Path 'HKLM:\SOFTWARE\Unattended' -Name domain -Value $domain
+   New-ItemProperty -Path 'HKLM:\SOFTWARE\Unattended' -Name dcusername -Value $dcusername
+   New-ItemProperty -Path 'HKLM:\SOFTWARE\Unattended' -Name dcpassword -Value $dcpassword
+   New-ItemProperty -Path 'HKLM:\SOFTWARE\Unattended' -Name setupPath  -Value $setupPath
+   New-ItemProperty -Path 'HKLM:\SOFTWARE\Unattended' -Name setupPath  -Value $dnsip
    Restart-And-Resume $script "Join"
 }
 
+if (!Should-Run-Step "Prepare"){
+    
+    $adminpassword = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Unattended' -Name domain).domain
+    $svcusername= (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Unattended' -Name dcusername).dcusername 
+    $svcpassword=(Get-ItemProperty -Path 'HKLM:\SOFTWARE\Unattended' -Name dcpassword).dcpassword
+    $features=(Get-ItemProperty -Path 'HKLM:\SOFTWARE\Unattended' -Name setupPath).setupPath
+    $instancename=(Get-ItemProperty -Path 'HKLM:\SOFTWARE\Unattended' -Name dnsip).dnsip
+}
+
+
 if (Should-Run-Step "Join") 
 {
-    $completeusername = $domain
+    log "joining domain"
     $secpasswd = ConvertTo-SecureString $dcpassword -AsPlainText -Force
     $creds = New-Object System.Management.Automation.PSCredential ("administrator" , $secpasswd)
 	Write-Host "Joining Active Directory"
@@ -109,35 +134,66 @@ if (Should-Run-Step "Join")
     $wmi = Get-WmiObject win32_networkadapterconfiguration -filter "ipenabled = 'true'"
     $wmi.SetDNSServerSearchOrder($dnsip)
     add-computer -Credential $creds -DomainName $domain
+    if (!$?) {
+        $errorMessage = ($error[0] | out-string)
+        log "Add to domain failed"
+        log $errorMessage
+        throw " failed to join AD"
+    }
+    log "Joined domain"
     Write-Host "System will be rebooting right now"
 	Restart-And-Resume $script "Features"
 }
 
 if (Should-Run-Step "Features") 
 {
+    log "installing windows features"
 	Write-Host "Installing Windows Features"
 	Install-WindowsFeature RSAT-ADDS, AS-HTTP-Activation, Desktop-Experience, NET-Framework-45-Features, RPC-over-HTTP-proxy, RSAT-Clustering, RSAT-Clustering-CmdInterface, Web-Mgmt-Console, WAS-Process-Model, Web-Asp-Net45, Web-Basic-Auth, Web-Client-Auth, Web-Digest-Auth, Web-Dir-Browsing, Web-Dyn-Compression, Web-Http-Errors, Web-Http-Logging, Web-Http-Redirect, Web-Http-Tracing, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Lgcy-Mgmt-Console, Web-Metabase, Web-Mgmt-Console, Web-Mgmt-Service, Web-Net-Ext45, Web-Request-Monitor, Web-Server, Web-Stat-Compression, Web-Static-Content, Web-Windows-Auth, Web-WMI, Windows-Identity-Foundation
-	Write-Host "System will be rebooting right now"
+	if (!$?) {
+        $errorMessage = ($error[0] | out-string)
+        log "windows features installation failed"
+        log $errorMessage
+        throw "Failed to add Windows Features"
+    }
+    log "Windows features installed"
+    Write-Host "System will be rebooting right now"
 	Restart-And-Resume $script "Prerequisites"
 }
 
 if (Should-Run-Step "Prerequisites") 
 {
+    log "Installing Exchange 2013 prerequisites"
 	Write-Host "Installing Exchange 2013 Prerequisites"
 	DependencyInstall "http://download.microsoft.com/download/0/A/2/0A28BBFA-CBFA-4C03-A739-30CCA5E21659/FilterPack64bit.exe" "FilterPack64bit.exe"	
 	DependencyInstall "http://download.microsoft.com/download/A/A/3/AA345161-18B8-45AE-8DC8-DA6387264CB9/filterpack2010sp1-kb2460041-x64-fullfile-en-us.exe" "filterpack2010sp1-kb2460041-x64-fullfile-en-us.exe"
 	DependencyInstall "http://download.microsoft.com/download/2/C/4/2C47A5C1-A1F3-4843-B9FE-84C0032C61EC/UcmaRuntimeSetup.exe" "UcmaRuntimeSetup.exe"
+    if (!$?) {
+        $errorMessage = ($error[0] | out-string)
+        log "failed to download and install prerequisites"
+        log $errorMessage
+        throw "Failed to install Exchange Prerequisites"
+    }
+    log "Prerequisites installed"
 	Write-Host "System will be rebooting right now"
 	Restart-And-Resume $script "Install"
 }
 
 if (Should-Run-Step "Install") 
 {
+    log "Exchange 2013 installing"
 	Write-Host "Installing Exchange 2013"
 	Start-Process -Wait -FilePath $setupPath -ArgumentList "/IAcceptExchangeServerLicenseTerms /ps"
 	Start-Process -Wait -FilePath $setupPath -ArgumentList "/IAcceptExchangeServerLicenseTerms /p /on:$env:USERDOMAIN"
 	Start-Process -Wait -FilePath $setupPath -ArgumentList "/IAcceptExchangeServerLicenseTerms /pd"
-	Start-Process -Wait -FilePath $setupPath -ArgumentList "/IAcceptExchangeServerLicenseTerms /mode:install /r:mb,ca /MdbName:exchange_db1"
+	Start-Process -Wait -FilePath $setupPath -ArgumentList "/IAcceptExchangeServerLicenseTerms /mode:install /r:mb,ca /MdbName:exchange2013_db1"
+    if (!$?) {
+        $errorMessage = ($error[0] | out-string)
+        log "Exchange 2013 installation failed"
+        log $errorMessage
+        throw "Failed to install Exchange 2013"
+    }
+    log "Exchange 2013 installed"
 	Write-Host "System will be rebooting right now"
 	Restart-And-Resume $script "Completing"
 }
@@ -149,7 +205,8 @@ if (Should-Run-Step "Completing")
     Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultUserName -Value ""
     Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultPassword -Value ""
     Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name DefaultDomainName -Value ""
-
+    Remove-Item -Path Registry::HKLM\SOFTWARE\Unattended
+    log "Removed reg keys"
 }
 
 Wait-For-Keypress "Exchange 2013 installation completed, press any key to exit ..."
